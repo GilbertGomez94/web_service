37011ef4405cfcf5a549e11a75006113e0fe4fd9
Gilbert Gómez <94gagb@gmail.com>
2019-09-21 23:09:09 -0400

    primer commit con proyecto creado

---
+++ adminvas/src/main/java/com/api/adminvas/AdminvasApplication.java
@@ -0,0 +1,13 @@
+package com.api.adminvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminvasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminvasApplication.class, args);
+    }
+
+}

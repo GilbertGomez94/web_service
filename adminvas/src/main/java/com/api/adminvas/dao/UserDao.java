ef699df11293064653194116795ecbdacd442b2c
Gilbert Gómez <94gagb@gmail.com>
2019-09-22 18:55:56 -0400

    Se agregan clases y demás para formar la base de la arquiectura

---
+++ adminvas/src/main/java/com/api/adminvas/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.api.adminvas.dao;
+
+import com.api.adminvas.domain.User;
+
+public class UserDao {
+
+    private User user;
+
+
+    public User getUser( User user ) throws Exception{
+        return user;
+    }
+}
